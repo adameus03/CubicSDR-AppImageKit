d0b10e079a0515269a04ffaee12fe1db5709a821
adma00x <112713711+adameus03@users.noreply.github.com>
2025-06-25 13:29:16 +0200

    Add missing packages - amendment

--- vagrant/scripts/bootstrap_centos.sh
+++ vagrant/scripts/bootstrap_centos.sh
@@ -5,7 +5,7 @@ sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 sudo dnf -y install epel-release
-sudo dnf -y install fuse git cmake mesa-libGL-devel gcc gcc-c++ automake cmake pulseaudio-libs-devel gtk3-devel freeglut freeglut-devel libusb1-devel libtool fftw-devel libarchive wget libcurl-devel ncurses-devel libxml2-devel bison flex libaio-dev
+sudo dnf -y install fuse git cmake mesa-libGL-devel gcc gcc-c++ automake cmake pulseaudio-libs-devel gtk3-devel freeglut freeglut-devel libusb1-devel libtool fftw-devel libarchive wget libcurl-devel ncurses-devel libxml2-devel bison flex libaio-devel
 
 sudo bash -c "echo /usr/local/lib64/ > /etc/ld.so.conf.d/CubicSDR.el8_3.x86_64.conf"
 
